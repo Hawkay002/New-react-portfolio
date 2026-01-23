5b26084ab7cd39d89e5285324a1fc0aca2ef55b8
Hawkay002 <119169431+Hawkay002@users.noreply.github.com>
2026-01-23 19:41:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -48,7 +48,7 @@ const db = getFirestore(app);
 // --- UPDATED COUNTRY DATA WITH ISO CODES ---
 const allCountries = [
   { name: "Afghanistan", dial_code: "+93", code: "AF" },
-  { name: "Aland Islands", dial_code: "+358", code: "AX" }
+  { name: "Aland Islands", dial_code: "+358", code: "AX" },
   { name: "Albania", dial_code: "+355", code: "AL" },
   { name: "Algeria", dial_code: "+213", code: "DZ" },
   { name: "American Samoa", dial_code: "+1684", code: "AS" },
