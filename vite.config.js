a08ef0c71530f45681a19bca07b1bce89da50a36
Hawkay002 <119169431+Hawkay002@users.noreply.github.com>
2026-01-29 12:06:45 +0530

    Define global constant for build date
    Add global constant for build date in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // This creates a global constant with the current date string
+    '__BUILD_DATE__': JSON.stringify(new Date().toLocaleDateString('en-GB', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric'
+    })),
+  },
 })
